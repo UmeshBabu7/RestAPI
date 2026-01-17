b1e2b3a11616a743a7466197ed39c354bb3ba370
Umesh <umeshtamang129@gmail.com>
2026-01-17 07:45:48 +0545

    first api testing views

--- restapiproject/restapi_app/views.py
+++ restapiproject/restapi_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import APIView
+from rest_framework import status
+
 
 # Create your views here.
+
+class TestView(APIView):
+    def get(self, request):
+        return JsonResponse({"messages":"api testing..."}, status=200)
+
+
+    def post(self, request):
+        data = request.data 
+        return JsonResponse({"messages":data}, status=200)
+    
